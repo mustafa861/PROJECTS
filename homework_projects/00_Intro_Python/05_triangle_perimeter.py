1af6dccce30441ef8ad43b7f316af5d61877220f
mustafa861 <mustafa1212adnan@gmail.com>
2025-04-04 20:38:39 +0500

    .

--- homework_projects/00_Intro_Python/05_triangle_perimeter.py
+++ homework_projects/00_Intro_Python/05_triangle_perimeter.py
@@ -1,19 +1,10 @@
-def main():
-    """A program to prompt the user to enter the lengths of each side of a triangle 
-    and then calculate and print the perimeter of the triangle."""
-    
-    # Taking input and converting to float
-    length_side1 = float(input("Enter first side length: "))
-    length_side2 = float(input("Enter second side length: "))
-    length_side3 = float(input("Enter third side length: "))
+  
+length_side1 = float(input("Enter first side length: "))
 
-    # Calculating perimeter
-    perimeter = length_side1 + length_side2 + length_side3
+length_side2 = float(input("Enter second side length: "))
 
-    # Displaying the result
-    print(f"\nThe perimeter of the triangle is {perimeter}\n")
+length_side3 = float(input("Enter third side length: "))
 
-# Ensuring the function runs only when the script is executed directly
-if __name__ == '__main__':
-    main()
- 
+perimeter = length_side1 + length_side2 + length_side3
+
+print(f"\nThe perimeter of the triangle is {perimeter}\n")
