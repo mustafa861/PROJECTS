65b7e791cfc135534fa3aee82c78594274295788
mustafa861 <mustafa1212adnan@gmail.com>
2025-04-04 23:07:01 +0500

    .

--- homework_projects/05_loops_control_flow/03_print_evens.py
+++ homework_projects/05_loops_control_flow/03_print_evens.py
@@ -1,9 +1,6 @@
 max_num=20
-""" a program to print even numers till  the range of 20"""
-def main():
-    for i in range(20):
+
+for i in range(20):
+        
         print(i*2)
-if __name__ == '__main__':
-     main()
-       
  
--- homework_projects/05_loops_control_flow/04_wholesome_machine.py
+++ homework_projects/05_loops_control_flow/04_wholesome_machine.py
@@ -1,16 +1,14 @@
 AFFIRMATION = "I am capable of doing anything I put my mind to."
 
-def main():
-    """Prompt the user to type the affirmation correctly until they get it right."""
-    print(f"Please type the following affirmation: {AFFIRMATION}")
+print(f"Please type the following affirmation: {AFFIRMATION}")
 
-    user_prompt = input()  # Get user input
+user_prompt = input()  
 
-    while user_prompt != AFFIRMATION:  # If the input is incorrect, keep asking
+while user_prompt != AFFIRMATION:  
+
         print("That's not the correct affirmation.")
-        user_prompt = input()  # Ask for input again
 
-    print("Now you typed the correct affirmation!")
+        user_prompt = input()  
 
-if __name__ == '__main__':
-    main()
+print("Now you typed the correct affirmation!")
+
