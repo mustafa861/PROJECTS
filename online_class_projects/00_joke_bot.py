d7c5e81312cc159ed8ffd94ca7c02d15d3f74c0c
mustafa861 <mustafa1212adnan@gmail.com>
2025-04-04 23:37:31 +0500

    Create 00_joke_bot.py

---
+++ online_class_projects/00_joke_bot.py
@@ -0,0 +1,18 @@
+
+prompt:str="What do you want to print?"
+
+joke:str="Why do Java developers wear glasses? Because they donot C#! 😆👓"
+
+sorry:str="Sorry ! I only print jokes"
+
+user_input=input(prompt)
+
+if user_input == "joke":
+
+        print(joke)
+
+else:
+
+        print(sorry)
+
+        
