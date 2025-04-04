d71623b2c1c590422d04a57dc93ffa4a2444ebe5
mustafa861 <mustafa1212adnan@gmail.com>
2025-04-04 22:33:47 +0500

    .

--- homework_projects/02_lists/06_get_last_element.py
+++ homework_projects/02_lists/06_get_last_element.py
@@ -1,17 +1,25 @@
+
 def user_list():
-    """Gets a list from the user and prints the last element."""
+    
     lst = []  
+
     while True:
+    
         element = input("Enter an element of the list or press Enter to stop: ")
+    
         if element == "":
-            break  # Stop taking input when the user presses Enter
+    
+            break  
+    
         lst.append(element)
+    
         print("List:", lst)  
 
     if lst:
-        print("Last Element:", lst[-1])  # Print the last element if the list is not empty
+    
+        print("Last Element:", lst[-1])  
     else:
-        print("The list is empty!")  # No need for extra logic here
+    
+        print("The list is empty!")  
 
-# Run the function
 user_list()
