739a34c4e578fedd41d36ebf48fbea6e23dce27f
mustafa861 <mustafa1212adnan@gmail.com>
2025-04-05 19:52:38 +0500

    Update 04_random_numbers.py

--- online_class_projects/01_basics/04_random_numbers.py
+++ online_class_projects/01_basics/04_random_numbers.py
@@ -2,7 +2,7 @@ import random
 
 def random_num(num, min_value=1, max_value=100):
 
-    return [random.randint(min_value, max_value)for i in range(num)]
+    return[random.randint(min_value, max_value)for i in range(num)]
 
 num = 10 
 
